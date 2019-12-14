2c01499a0e91ff1d0480de01d6e599cdb7d7f2c6
jsifferman <jim.a.sifferman@gmail.com>
2019-12-14 15:59:19 -0500

    Added dictionary checking. Cleaned up stale code.

---
+++ assignments/9/js/utility.js
@@ -0,0 +1,88 @@
+var ScrabbleTiles = [] ;
+ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12 } ;
+ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3  } ;
+ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8  } ;
+ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+
+var tileDistribution = [
+    "A", "A", "A", "A", "A", "A", "A", "A", "A",
+    "B", "B",
+    "C", "C",
+    "D", "D", "D", "D",
+    "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+    "F", "F",
+    "G", "G", "G",
+    "H", "H",
+    "I", "I", "I", "I", "I", "I", "I", "I", "I",
+    "J",
+    "K",
+    "L", "L", "L", "L",
+    "M", "M",
+    "N", "N", "N", "N", "N", "N",
+    "O", "O", "O", "O", "O", "O", "O", "O",
+    "P", "P",
+    "Q",
+    "R", "R", "R", "R", "R", "R",
+    "S", "S", "S", "S",
+    "T", "T", "T", "T", "T", "T",
+    "U", "U", "U", "U",
+    "V", "V",
+    "W", "W",
+    "X",
+    "Y", "Y",
+    "Z",
+    "_", "_",
+];
+
+var tileFile = {
+    "A" : "Scrabble_Tile_A.jpg",
+    "B" : "Scrabble_Tile_B.jpg",
+    "C" : "Scrabble_Tile_C.jpg",
+    "D" : "Scrabble_Tile_D.jpg",
+    "E" : "Scrabble_Tile_E.jpg",
+    "F" : "Scrabble_Tile_F.jpg",
+    "G" : "Scrabble_Tile_G.jpg",
+    "H" : "Scrabble_Tile_H.jpg",
+    "I" : "Scrabble_Tile_I.jpg",
+    "J" : "Scrabble_Tile_J.jpg",
+    "K" : "Scrabble_Tile_K.jpg",
+    "L" : "Scrabble_Tile_L.jpg",
+    "M" : "Scrabble_Tile_M.jpg",
+    "N" : "Scrabble_Tile_N.jpg",
+    "O" : "Scrabble_Tile_O.jpg",
+    "P" : "Scrabble_Tile_P.jpg",
+    "Q" : "Scrabble_Tile_Q.jpg",
+    "R" : "Scrabble_Tile_R.jpg",
+    "S" : "Scrabble_Tile_S.jpg",
+    "T" : "Scrabble_Tile_T.jpg",
+    "U" : "Scrabble_Tile_U.jpg",
+    "V" : "Scrabble_Tile_V.jpg",
+    "W" : "Scrabble_Tile_W.jpg",
+    "X" : "Scrabble_Tile_X.jpg",
+    "Y" : "Scrabble_Tile_Y.jpg",
+    "Z" : "Scrabble_Tile_Z.jpg",
+    "_" : "Scrabble_Tile_Blank.jpg",
+}
